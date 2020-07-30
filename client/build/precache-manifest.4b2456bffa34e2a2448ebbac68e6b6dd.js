0c989d248798006c75e3e3ee2ad08c5ae517335f
王泽强 <zeqiang.wang@alibaba-inc.com>
2020-07-30 10:49:03 +0800

    add build

---
+++ client/build/precache-manifest.4b2456bffa34e2a2448ebbac68e6b6dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f562736353f080135bc5374ace65576",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bc5d67ecaaea628b9e2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bc5d67ecaaea628b9e2",
+    "url": "/static/js/main.82855b8d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
